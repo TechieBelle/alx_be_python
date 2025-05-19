8ba2cd9722497c7ea15e9e97fc132902388f0089
Folashade Bello <folashadebello.b@gmail.com>
2025-05-19 16:12:09 +0100

    basic operations program

---
+++ basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+sum = number1 + number2
+diff = number1 - number2
+product = number1 * number2
+print(f"Addition of {number1} and {number2} = {sum}")
+print(f"subtraction of {number1} and {number2} = {diff}")
+print(f"Multiplication of {number1} and {number2} = {product}")
