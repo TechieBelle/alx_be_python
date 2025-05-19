95a9d1b9254462bcbaecc34ce6de4a4983bffec7
Folashade Bello <folashadebello.b@gmail.com>
2025-05-19 16:45:04 +0100

    changed formula

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,5 +1,9 @@
 principal = 1000
 rate = 0.05
 time = 3
-interest = (principal*rate*time)
+P = principal 
+R = rate
+T = time 
+interest = (P*R*T)
+I = interest
 print(f"The simple interest is {interest}")
