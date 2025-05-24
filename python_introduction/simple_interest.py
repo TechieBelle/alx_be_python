42217956152eb3e8fb2f02cd670ed6f0355a6cfc
Folashade Bello <folashadebello.b@gmail.com>
2025-05-24 11:39:17 +0100

    commit all

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,9 +1,5 @@
 principal = 1000
 rate = 0.05
 time = 3
-P = principal 
-R = rate
-T = time 
-interest = (P*R*T)
-I = interest
+interest = principal*rate*time
 print(f"The simple interest is {interest}")
