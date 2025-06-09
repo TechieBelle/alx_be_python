59aafac58f58593addc7ad9d675ab5139aa36666
Folashade Bello <folashadebello.b@gmail.com>
2025-06-09 10:02:32 +0100

    changed Match cases to If and elif statements

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -7,21 +7,19 @@ num2 = float(input('Enter the second number: '))
 operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
 
 def perform_operation(num1, num2, operation):
-    match operation:
-        case "add":
-            return num1 + num2
-        case "subtract":
-            return num1 - num2
-        case "multiply":
-            return num1 * num2
-        case "divide":
-            if num2 == 0:
-                return num1 / num2
-            else:
-                return "Cannot divide by zero."
-        case _:
-            return "Invalid operation."
-
+    if operation == "add":
+        return num1 + num2
+    elif operation == "subtract":
+        return num1 - num2
+    elif operation == "multiply":
+        return num1 * num2
+    elif operation == "divide":
+        if num2 == 0:
+            return num1 / num2
+        else:
+            return "Error: Cannot divide by zero."
+    else:
+        return "Error: Invalid operation."
 
 
 # Output
