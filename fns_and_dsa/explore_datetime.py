290ae8bdabb9303dceecb43a5611aaedd10b70ec
Folashade Bello <folashadebello.b@gmail.com>
2025-06-09 15:30:16 +0100

    explore date and time

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,33 @@
+
+import datetime
+
+from sqlalchemy.sql.functions import current_date
+
+
+def display_current_datetime():
+    current_date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    print(f"Current date and time:{current_date}")
+
+#call function
+display_current_datetime()
+print()
+
+#Part 2: Calculate a Future Date
+def calculate_future_date():
+    # Ask the user for number of days
+    duration = int(input("Enter the number of days to add to the current date: "))
+
+    # Get today's date and time
+    current_date = datetime.datetime.now()
+
+    # Calculate future date
+    future_date = current_date + datetime.timedelta(days = duration)
+
+    print("Future date:", future_date.strftime("%Y-%m-%d"))
+
+
+#call function
+calculate_future_date()
+
+
+
